9e1aa2d68d8eb40b832df8b0d179e87d78a17e1e
nugyflex <jransome@creativesoftware.com.au>
2017-08-18 15:22:44 +1000

    Added new player animations

--- Vulkan2D/Player.cpp
+++ Vulkan2D/Player.cpp
@@ -6,7 +6,12 @@ Player::Player(int _x, int _y, Input* _inputPointer, Graphics* _graphicsPointer)
 	boundingBox.y = _y;
 	inputPointer = _inputPointer;
 	graphicsPointer = _graphicsPointer;
-	walkCycle.init(_graphicsPointer, glm::vec2(20, 225), 13, 25, 7, 8, 7);
+	walkCycleHorizontal.init(_graphicsPointer, glm::vec2(20, 225), 13, 25, 6, 8, 5);
+	walkCycleVerticleUp.init(_graphicsPointer, glm::vec2(20, 275), 13, 25, 6, 8, 5);
+	walkCycleVerticleDown.init(_graphicsPointer, glm::vec2(20, 250), 13, 25, 6, 8, 5);
+	standingHorizontal.init(_graphicsPointer, glm::vec2(124, 225), 13, 25, 6, 1, 5);
+	standingVerticleUp.init(_graphicsPointer, glm::vec2(124, 275), 13, 25, 6, 1, 5);
+	standingVerticleDown.init(_graphicsPointer, glm::vec2(124, 250), 13, 25, 6, 1, 5);
 	velocity = 8;
 }
 
@@ -24,6 +29,7 @@ void Player::handleInput()
 	if (inputPointer->keys.a) {
 		if (!inputPointer->keys.d) {
 			boundingBox.xv = -velocity;
+			lastDirection = left;
 		}
 		else {
 			boundingBox.xv = 0;
@@ -31,6 +37,7 @@ void Player::handleInput()
 	}
 	else if (inputPointer->keys.d) {
 		boundingBox.xv = velocity;
+		lastDirection = right;
 	}
 	else {
 		boundingBox.xv = 0;
@@ -38,6 +45,7 @@ void Player::handleInput()
 	if (inputPointer->keys.w) {
 		if (!inputPointer->keys.s) {
 			boundingBox.yv = -velocity;
+			lastDirection = up;
 		}
 		else {
 			boundingBox.yv = 0;
@@ -45,6 +53,7 @@ void Player::handleInput()
 	}
 	else if (inputPointer->keys.s) {
 		boundingBox.yv = velocity;
+		lastDirection = down;
 	}
 	else {
 		boundingBox.yv = 0;
@@ -59,11 +68,44 @@ void Player::updatePosition()
 
 void Player::draw()
 {
+
 	if (boundingBox.xv > 0) {
-		walkCycle.run();
-		walkCycle.draw(boundingBox.x, boundingBox.y);
+		walkCycleHorizontal.run();
+		walkCycleHorizontal.draw(boundingBox.x, boundingBox.y);
 	} else 	if (boundingBox.xv < 0) {
-		walkCycle.run();
-		walkCycle.drawFlipped(boundingBox.x, boundingBox.y);
+		walkCycleHorizontal.run();
+		walkCycleHorizontal.drawFlipped(boundingBox.x, boundingBox.y);
+	}
+	else {
+		walkCycleHorizontal.setFrame(0);
+		if (boundingBox.yv > 0) {
+			walkCycleVerticleDown.run();
+			walkCycleVerticleDown.draw(boundingBox.x, boundingBox.y);
+			walkCycleHorizontal.setFrame(0);
+			walkCycleVerticleUp.setFrame(0);
+		}
+		else if (boundingBox.yv < 0) {
+			walkCycleHorizontal.setFrame(0);
+			walkCycleVerticleDown.setFrame(0);
+			walkCycleVerticleUp.run();
+			walkCycleVerticleUp.draw(boundingBox.x, boundingBox.y);
+		}
+		else {
+			walkCycleHorizontal.setFrame(0);
+			walkCycleVerticleUp.setFrame(0);
+			walkCycleVerticleDown.setFrame(0);
+			if (lastDirection == right) {
+				standingHorizontal.draw(boundingBox.x, boundingBox.y);
+			}
+			else if (lastDirection == left) {
+				standingHorizontal.drawFlipped(boundingBox.x, boundingBox.y);
+			}
+			else if (lastDirection == up) {
+				standingVerticleUp.draw(boundingBox.x, boundingBox.y);
+			}
+			else if (lastDirection == down) {
+				standingVerticleDown.draw(boundingBox.x, boundingBox.y);
+			}
+		}
 	}
 }
--- Vulkan2D/Player.h
+++ Vulkan2D/Player.h
@@ -3,7 +3,7 @@
 #include "Graphics.h"
 #include "Input.h"
 #include "SpriteSheet.h"
-
+enum LastDirection {up, down, left, right};
 class Player {
 public:
 	Player(int _x, int _y, Input* _inputPointer, Graphics* _graphicsPointer);
@@ -13,9 +13,15 @@ public:
 	void updatePosition();
 	void draw();
 private:
-	SpriteSheet walkCycle;
+	SpriteSheet walkCycleHorizontal;
+	SpriteSheet walkCycleVerticleUp;
+	SpriteSheet walkCycleVerticleDown;
+	SpriteSheet standingHorizontal;
+	SpriteSheet standingVerticleUp;
+	SpriteSheet standingVerticleDown;
 	Input* inputPointer;
 	Graphics* graphicsPointer;
 	BoundingBox boundingBox;
 	float velocity;
+	LastDirection lastDirection;
 };
