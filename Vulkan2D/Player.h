8b8e54dbb688d40b462bb999e4e029447b070f05
jackransome <jransome@creativesoftware.com.au>
2017-08-21 23:57:25 +1000

    Revert "Revert "Added new player animations""
    This reverts commit a9fe67281436ea07168854a407981b2e5d48c948.

--- Vulkan2D/Player.h
+++ Vulkan2D/Player.h
@@ -2,7 +2,7 @@
 #include "BoundingBox.h"
 #include "Input.h"
 #include "SpriteSheet.h"
-
+enum LastDirection {up, down, left, right};
 class Player {
 public:
 	Player(int _x, int _y, Input* _inputPointer, Graphics* _graphicsPointer);
@@ -12,9 +12,15 @@ public:
 	void updatePosition();
 	void draw();
 private:
-	SpriteSheet walkCycle;
+	SpriteSheet walkCycleHorizontal;
+	SpriteSheet walkCycleVerticleUp;
+	SpriteSheet walkCycleVerticleDown;
+	SpriteSheet standingHorizontal;
+	SpriteSheet standingVerticleUp;
+	SpriteSheet standingVerticleDown;
 	Input* inputPointer;
 	Graphics* graphicsPointer;
 	BoundingBox boundingBox;
 	float velocity;
+	LastDirection lastDirection;
 };
